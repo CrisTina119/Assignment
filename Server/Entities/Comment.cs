d64737ccad64648a28e669db5d0a392e312a8120
CrisTina119 <cristinalincu19@gmail.com>
2025-09-11 12:36:19 +0200

    adds

--- Server/Entities/Comment.cs
+++ Server/Entities/Comment.cs
@@ -2,10 +2,12 @@ public class Comment
 {
     public int? Id { get; set; }
         public string? Body { get; set; }
+        public int? UserId { get; set; }
+        public int? PostId { get; set; }
 
 public Comment()
     {
-        
+
     }
 
 }
--- Server/Entities/User.cs
+++ Server/Entities/User.cs
@@ -2,7 +2,7 @@ public class User
 {
     public int? Id { get; set; }
         public string? userName { get; set; }
-        public string? password { get; set; }
+        public int? password { get; set; }
 
     public User()
     {
